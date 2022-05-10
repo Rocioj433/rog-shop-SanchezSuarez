4f95f9f1bf1aaec2f0c9f9dc176e48b502365844
Rocio <rocioj433@gmail.com>
2022-05-10 19:19:24 -0300

    confirmar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <ul>
+                <li>Menu</li>
+                <li>Shop</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
