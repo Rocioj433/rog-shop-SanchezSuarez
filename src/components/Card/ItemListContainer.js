91c984584d1cf83c8738404db0f9dd2992c69dd6
Rocio <rocioj433@gmail.com>
2022-05-14 20:16:27 -0300

    itemlistContainer changes

--- src/components/Card/ItemListContainer.js
+++ src/components/Card/ItemListContainer.js
@@ -3,9 +3,8 @@ import CardStyle from '../Card/CardStyle.css'
 
 const ItemListContainer = ({title}) => {
   return (
-    <div>
+    
         <h1 className='title'>{title}</h1>
-    </div>
   )
 }
 export default ItemListContainer
